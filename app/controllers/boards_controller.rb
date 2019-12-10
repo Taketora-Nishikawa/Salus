f20302205219df2ac9292b53f7c2068fe23334f2
”Taketora-Nishikawa” <milkreap@gmail.com>
2019-12-10 12:31:41 +0000

    ナビバー仮実装

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,7 +2,8 @@ class BoardsController < ApplicationController
    before_action :set_target_board, only: %i[show edit update destroy]
    
   def index
-    @boards = Board.page(params[:page])
+    @boards = params[:tag_id].present? ?Tag.find(params[:tag_id]).boards : Board.all
+    @boards = @boards.page(params[:page])
   end
   
   def new
@@ -41,7 +42,7 @@ class BoardsController < ApplicationController
   end
   
   def destroy
-    @board.delete
+    @board.destroy
     
     redirect_to boards_path, flash: { notice: "「#{@board.title}」の投稿が削除されました"}
   end
@@ -49,7 +50,7 @@ class BoardsController < ApplicationController
   private
   
   def board_params
-    params.require(:board).permit(:name, :title, :body)
+    params.require(:board).permit(:name, :title, :body, tag_ids: [])
   end
   
   def set_target_board
